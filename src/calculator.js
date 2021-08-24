6ace2f92bbcb9777e3e8c39a501b8bd6a34da356
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-24 18:08:43 -0300

    Add calculator tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,20 @@
-class Calculator {
-  add (num1, num2) {
-    return num1 + num2
-  }
-
-  subtract (num1, num2) {
-    return num1 - num2
-  }
-
-  divide (num1, num2) {
-    return num1 / num2
-  }
-
-  multiply (num1, num2) {
-    return num1 * num2
-  }
+function add (num1, num2) {
+  return num1 + num2
 }
+
+function subtract (num1, num2) {
+  return num1 - num2
+}
+
+function divide (num1, num2) {
+  return num1 / num2
+}
+
+function multiply (num1, num2) {
+  return num1 * num2
+}
+
+exports.add = add;
+exports.subtract = subtract;
+exports.divide = divide;
+exports.multiply = multiply;
